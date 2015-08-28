92ca6a82decbd4ae07e8179a07f5187577b502c2
Alonso <alons182@hotmail.com>
2015-08-28 15:15:23 -0600

    cambios ortograficos

--- resources/views/pages/fake-email.blade.php
+++ resources/views/pages/fake-email.blade.php
@@ -10,7 +10,7 @@
             <ul>
                 <li>Revisá a donde te envían los enlaces y/o botones del e-mail que recibiste. Nuestro sitio cuenta con una conexión segura HTTPS; Revisa que salga el candado verde y además verificá que realmente te lleven GuanacasteVende.com (<a
                             href="http://www.guanacastevende.com">www.guanacastevende.com</a>)</li>
-                <li>Si te llevan a una web diferente denúncia el email falso (enlace a <a href="mailto::soporte@gunacastevende.com">soporte@gunacastevende.com</a>)</li>
+                <li>Si te llevan a una web diferente denúncia el email falso (enlace a <a href="mailto::soporte@guanacastevende.com">soporte@guanacastevende.com</a>)</li>
             </ul>
         <div class="tips__gallery txt-center">
             <img src="/img/recibi-email-falso-gte-vende.jpg" alt="Recibi email falso" class="img" />
@@ -21,7 +21,7 @@
         <h3>Para que lo sepas:</h3>
         <ul>
             <li>Nunca te pediremos información personal, como tu número de tarjeta de crédito, identificación, claves de seguridad o número de seguridad fiscal, por e-mail o por teléfono.</li>
-            <li>Si te avisamos por e-mail que tu cuenta y/o usuario está deshabilitado y al instante recibes otro e-mail que indica lo contrario, no avances con la operación, mejor avísanos (enlace a <a href="mailto::soporte@gunacastevende.com">soporte@gunacastevende.com</a>)</li>
+            <li>Si te avisamos por e-mail que tu cuenta y/o usuario está deshabilitado y al instante recibes otro e-mail que indica lo contrario, no avances con la operación, mejor avísanos (enlace a <a href="mailto::soporte@guanacastevende.com">soporte@guanacastevende.com</a>)</li>
         </ul>
 
 
