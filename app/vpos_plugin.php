a9a306e6ab1078094562f5f0197549f03a0c2ea8
Alonso <alons182@hotmail.com>
2015-10-15 22:47:46 -0600

    test banco

--- app/vpos_plugin.php
+++ app/vpos_plugin.php
@@ -509,11 +509,12 @@
 					$arregloSalida['tax_'.$i.'_amount'] = $nodoTax->getAttribute('amount');
 					$i++;
 
-					while (($nodoTax=$nodoTax->nextSibling)!=null) {
-						$arregloSalida['tax_'.$i.'_name'] = $nodoTax->getAttribute('name');
-						$arregloSalida['tax_'.$i.'_amount'] = $nodoTax->getAttribute('amount');
-						$i++;
-					}
+
+				}
+				while (($nodoTax=$nodoTax->nextSibling)!=null) {
+					$arregloSalida['tax_'.$i.'_name'] = $nodoTax->getAttribute('name');
+					$arregloSalida['tax_'.$i.'_amount'] = $nodoTax->getAttribute('amount');
+					$i++;
 				}
 
 
