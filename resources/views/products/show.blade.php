02ac90a03966bdedf5444a02c7685ac926f5b54c
Alonso <alons182@hotmail.com>
2015-10-30 09:30:08 -0600

    test share

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,11 +4,11 @@
     {!! trim($tag->name) !!}@endforeach
 @stop
 @section('meta-share')
-    <!--<meta property="og:title" content="Guanacaste Vende | {!! $product->name !!}" />
+    <meta property="og:title" content="Guanacaste Vende | {!! $product->name !!}" />
     <meta property="og:description" content="{!! trim(strip_tags( $product->description ))!!} | @foreach ($product->tags as $tag)
     {!! trim($tag->name) !!}@endforeach" />
     <meta property="og:image" content="{!! photos_path('products').'thumb_'.$product->image !!}" />
-    <meta property="og:url" content="{!! Request::url() !!}"/>-->
+    <meta property="og:url" content="{!! Request::url() !!}"/>
 @stop
 
 @section('content')
