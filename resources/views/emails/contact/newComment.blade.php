df0ff694aec81203493b2ee37fcd696a61be7f2d
Alonso <alons182@hotmail.com>
2015-12-02 11:06:45 -0600

    se cambio email por username en comentario y se agrego redes al email

--- resources/views/emails/contact/newComment.blade.php
+++ resources/views/emails/contact/newComment.blade.php
@@ -5,8 +5,26 @@
     <title>Información desde el sitio Guanacaste Vende - Nuevo Comentario Publicado</title>
 </head>
 <body>
+<div class="logo" style="text-align: center;">
+    <img src="{{ url('/img/logo-email.png') }}" alt="Guanacaste Vende">
+</div>
 <p>Nuevo Comentario publicado en  tu producto: {{ $product['name'] }}</p>
 <p>Puedes revisarlo y contestarlo en el siguiente enlace: <a href="{{ url('/products/'.$product['id']) }}" target="_blank">aquí</a></p>
+<p> Seguinos en nuestras redes sociales para estar atento a las sorpresas !!</p>
+<a style="display: inline-block; padding: 5px;" href="https://www.facebook.com/guanacastevende" target="_blank"><img src="{{ url('/img/facebook.jpg') }}"
+                                                                                                                     alt="Facebook"></a>
+<a style="display: inline-block; padding: 5px;" href="https://twitter.com/GuanacasteVende" target="_blank"><img src="{{ url('/img/twitter.jpg') }}"
+                                                                                                                alt="Twitter"></a>
+<a style="display: inline-block; padding: 5px;" href="https://www.youtube.com/channel/UCVDiC3vIclXSKmrKViPIIag" target="_blank"><img src="{{ url('/img/youtube.jpg') }}"
+                                                                                                                                     alt="Youtube"></a>
+<a style="display: inline-block; padding: 5px;" href="https://plus.google.com/+GuanacastevendeCR" target="_blank"><img src="{{ url('/img/googleplus.jpg') }}"
+                                                                                                                       alt="Google Plus"></a>
+<p>
+<a style="display: inline-block; padding: 5px; vertical-align: middle;" href="https://www.mastercard.us/en-us/merchants/safety-security/securecode.html" target="_blank"><img
+            src="{{ url('/img/logo-mastercard.png') }}" alt="Mastercard" /></a>
+<a style="display: inline-block; padding: 5px; vertical-align: middle;" href="https://usa.visa.com/personal/security/vbv/index.html?it=wb|/|Learn%20More" target="_blank"><img
+            src="{{ url('/img/logo-verified-by-visa.png') }}" alt="Verified by Visa" /></a>
+</p>
 
 </body>
 </html>
