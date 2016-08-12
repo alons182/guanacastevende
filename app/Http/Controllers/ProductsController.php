1d72f9e5dd2dedf75c9bc27e15d9abedc3d54380
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-08-12 12:06:23 -0600

    test fix category ancestors

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -108,7 +108,7 @@ class ProductsController extends Controller {
 
     public function search(Request $request, $category = null)
     {
-        dd($category);
+        //dd($category);
         $search = array_add($request->all(), 'published', [1,4]);
 
         //if ($search['q'] == '') return view('categories.index');
@@ -118,7 +118,7 @@ class ProductsController extends Controller {
             list($products, $category) = $this->productRepository->findByCategory($category);
 
         $q = (isset($search['q'])) ? $search['q'] : '';
-
+        dd($products->toArray());
         return view('products.index')->with(compact('products', 'q', 'category'));
     }
 
