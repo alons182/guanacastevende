4f3ba37b2411c34bec8e31bb289fb05b4b1f621f
Alonso Avotz <alonso@avotz.com>
2017-03-27 17:29:40 -0600

    se quito algunos banners

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,26 +13,23 @@
                 <div class="cycle-prev"><i class="icon-angle-left"></i></div>
                 <div class="cycle-next"><i class="icon-angle-right"></i></div>
 
-                <div class="banner__slide" style="background-image: url('/img/rifa-entradas-cine.jpg')">
+                <!-- <div class="banner__slide" style="background-image: url('/img/rifa-entradas-cine.jpg')">
 
                     <a href="https://www.facebook.com/guanacastevende" class="banner__slide__link" target="_blank"></a>
-                </div>
+                </div> -->
                 <div class="banner__slide" style="background-image: url('/img/gte-vende-banner-ya-esta-vendiendo.jpg')">
 
                     <a href="#" class="banner__slide__link" ></a>
                 </div>
-                <div class="banner__slide" style="background-image: url('/img/gte-vende-banner-publicitario-Bortex.jpg')">
-
-                    <a href="#" class="banner__slide__link" ></a>
-                </div>
+                
                 <!--<div class="banner__slide" style="background-image: url('/img/banner.jpg')">
                        
                         <a href="/about" class="banner__slide__link"></a>
                     </div>-->
-                <div class="banner__slide" style="background-image: url('/img/banner2.jpg')">
+                <!-- <div class="banner__slide" style="background-image: url('/img/banner2.jpg')">
 
                     <a href="https://www.avotz.com" class="banner__slide__link" target="_blank"></a>
-                </div>
+                </div> -->
 
 
 
